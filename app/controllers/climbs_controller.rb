4cc218e64a81ce6a2fefd5865c23b2152f53e9b3
MarioRoncador <mroncador@alaska.edu>
2017-09-18 23:45:04 -0400

    Search works with date and mountain

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -3,11 +3,32 @@ class ClimbsController < ApplicationController
 
   def index
     # @climbs = Climb.all.order("#{sort_column} #{sort_direction}")
+
+    filter = {}
     if params[:search]
-      @climbs = Climb.search(params[:search]).order("#{sort_column} #{sort_direction}").paginate(:page => params[:page], :per_page => 12)
-    else
-      @climbs = Climb.all.order("#{sort_column} #{sort_direction}").paginate(:page => params[:page], :per_page => 12)
+      filter = filter.merge(mountains: {name: params[:search]})
     end
+    if params[:date]
+      date = params[:date]
+      split = date.to_s.split('/')
+      result = split[2]+"-"+split[0]+"-"+split[1] #Retrieve date in the correct format
+      filter = filter.merge(date: result)
+    end
+
+    @climbs = Climb.joins(:mountain)
+      .where(filter)
+      .order("#{sort_column} #{sort_direction}")
+      .paginate(:page => params[:page], :per_page => 12)
+
+
+    # if params[:search] && params[:date]
+    #   @climbs = Climb.joins(:mountains).order("#{sort_column} #{sort_direction}").paginate(:page => params[:page], :per_page => 12)
+    #   @climbs = @climbs.where(name: params[:search])
+    #   @climbs = @climbs.where(date: params[:date])
+    #   @climbs #= @climbs.where(filter)
+    # else
+    #   @climbs = Climb.all.order("#{sort_column} #{sort_direction}").paginate(:page => params[:page], :per_page => 12)
+    # end
   end
 
   def show
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,10 @@ class HomeController < ApplicationController
 
   def index
     if params[:search] && params[:date]
-      @climbs = Climb.search(params[:search]).order("date ASC").paginate(:page => params[:page], :per_page => 12)
+      @climbs = Climb.order("date ASC").paginate(:page => params[:page], :per_page => 12)
+      @climbs = @climbs.where(params[:search])
+      @climbs = @climbs.where(params[:date])
+      @climbs
     else
       @climbs = Climb.all.order("date ASC").paginate(:page => params[:page], :per_page => 12)
     end
